dd50c778be75b1f985b81643795b91749dbc74fa
BenPease21 <BAJA154@outlook.com>
2025-02-16 18:07:03 -0600

    Create Question1.cs

---
+++ Question1.cs
@@ -0,0 +1,28 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace StringWordCount
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            string sentance = "Just an example here move along";
+            Console.WriteLine(WordCounter.CountWords(sentance));
+        }
+    }
+
+
+    public class WordCounter
+    {
+        public static int CountWords(string sentance)
+        {
+            string[] words = sentance.Split(' ', ',');
+            return words.Length;
+        }
+    }
+
+
+
+
+}
+
