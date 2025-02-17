6a414fc44efd633c82c0a45916a9a3187e23df15
BenPease21 <BAJA154@outlook.com>
2025-02-16 18:45:30 -0600

    Create Question 3.cs

---
+++ Question 3.cs
@@ -0,0 +1,28 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using Microsoft.VisualBasic;
+
+namespace Question3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string msg = "123";
+            Console.WriteLine(StringOrderChecker.IsOrder(msg));
+        }
+    }
+
+    public class StringOrderChecker
+    {
+        public static bool IsOrder(string str)
+        {
+            for (int i = 1; i < str.Length; i++)
+            {
+                if (str[i] < str[i - 1]) { return false; }
+
+            }
+
+            return true;
+        }
+    }
+}
