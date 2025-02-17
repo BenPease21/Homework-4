2ea36569cc7439b55c4160350b8affeef18903f2
BenPease21 <BAJA154@outlook.com>
2025-02-16 18:25:30 -0600

    Create Question 2.cs

---
+++ Question 2.cs
@@ -0,0 +1,44 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace StringWordCount
+{
+    public class Program
+    {
+        public static void Main()
+        {
+           string input = Console.ReadLine();
+           Console.WriteLine(FlipFirstAndLast.Flipper(input));
+        }
+    }
+
+
+    public class FlipFirstAndLast
+    {
+
+        public static string Flipper(string sentance)
+        {
+
+            string NewString;
+
+            if (sentance.Length < 2) { return "Incompatible."; }
+
+            if (sentance[0] == sentance[sentance.Length - 1]) return "Two's a pair.";
+
+
+                char[] charArray = sentance.ToCharArray();
+                char first = charArray[0];
+                char last = charArray[sentance.Length - 1];
+
+                charArray[0] = last;
+                charArray[sentance.Length - 1] = first;
+                return new string(charArray);
+
+
+        }
+    }
+
+
+
+
+}
+
